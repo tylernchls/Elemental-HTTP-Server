303e2cc469765d17029094c9e34764de46557ee3
Tyler Nichols <tylernchls@gmail.com>
2016-12-30 15:39:55 -1000

    working on sending css file with conditionals

--- server.js
+++ server.js
@@ -3,13 +3,23 @@ const fs = require('fs');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  let url = req.url.trim().split('/').join('');
+  let url = req.url.substr(1);
   console.log(url);
+
+
 
   fs.readFile(`./public/${url}`, (err, file) => {
     if (err) throw err;
-    res.write(file);
-    res.end();
+    if(url.endsWith('.css')){
+      res.setHeader('Content-Type', 'text/css');
+      res.write(file);
+      res.end();
+    } else {
+      res.setHeader('Content-Type', 'text/html');
+      res.write(file);
+      res.end();
+    }
+
   });
 
 });
