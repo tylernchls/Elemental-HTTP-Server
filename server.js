b8c35d94ded8d97d884aa1808a53052f50e88fa0
Tyler Nichols <tylernchls@gmail.com>
2016-12-30 14:17:54 -1000

    added url variable that will grab the file that is being requested and passed into readFile, then sent back in response

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const http = require('http');
+const fs = require('fs');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end();
+  let url = req.url.trim().split('/').join('');
+  console.log(url);
+
+  fs.readFile(`./public/${url}`, (err, file) => {
+    if (err) throw err;
+    res.write(file);
+    res.end();
+  });
+
 });
+
 
 
 
