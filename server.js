a6b2f19ea7dcae4ed5b0030ba6d45d6c4a8a219e
Tyler Nichols <tylernchls@gmail.com>
2017-01-02 11:27:27 -1000

    if file doesnt exist will create that file otherwise just read the file and return the data

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ let fileType;
 
 const server = http.createServer((req, res) => {
   let url = req.url;
-  console.log(url);
   let findFileType = url.split('').splice(-3,3).join('');
 
   // finds fileType ending and sets it
@@ -17,91 +16,97 @@ const server = http.createServer((req, res) => {
     fileType = 'html';
   }
 
+    /*
+    checks if url passed in exits or not, if does exist will read file and send data out.
+    if file doens't exist will create the file
+    */
 
-    fs.readFile(`./public/${url}`, (err, fileContent) => {
 
-      if (url === '/') {
+    fs.readFile(`./public/${url}`, (err, files) => {
+      if(err) {
+        console.log('this file doesnt exist');
 
-        fs.readFile('./public/index.html', (err, fileContent) => {
-          console.log('testing');
 
-          res.writeHead(200, {'Content-Type': `text/${fileType}`, 'Content-Length': `${fileContent.length}`});
-          res.write(fileContent);
-          res.end();
+        req.on('data', (data) => {
 
+          var dataPost = querystring.parse(data.toString());
+          console.log(dataPost);
+
+          var fileContents = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title>The Elements - ${dataPost.elementName}</title>
+    <link rel="stylesheet" href="/css/styles.css">
+    <link href = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/BristolStoolChart.png/1024px-BristolStoolChart.png">
+    </head>
+    <body>
+    <h1>${dataPost.elementName}</h1>
+    <h2>${dataPost.elementSymbol}</h2>
+    <h3>${dataPost.elementAtomicNumber}</h3>
+    <p>${dataPost.elementDescription}</p>
+    <p><a href="/">back</a></p>
+    </body>
+    </html>`
+
+
+        fs.writeFile(`./public/${dataPost.elementName}.html`, fileContents, (err) => {
+          if (err) throw err;
+          res.writeHead(200, {'Content-Type' : 'application/json'});
+          res.end({'success': 'true'});
         });
 
-      } else if(err) {
-
-        fs.readFile('./public/404.html', (err, fileContent) => {
-
-          res.writeHead(404, {'Content-Type': `text/${fileType}`, 'Content-Length': `${fileContent.length}`});
-          res.write(fileContent);
-          res.end();
 
         });
 
       } else {
 
-         res.writeHead(200, {'Content-Type': `text/${fileType}`, 'Content-Length': `${fileContent.length}`});
-         res.write(fileContent);
-         res.end();
+        fs.readFile(`./public/${url}`, (err, fileContent) => {
+          // takes back to index.html page
+          if (url === '/') {
 
+            fs.readFile('./public/index.html', (err, fileContent) => {
+              console.log('testing');
+
+              res.writeHead(200, {'Content-Type': `text/${fileType}`, 'Content-Length': `${fileContent.length}`});
+              res.write(fileContent);
+              res.end();
+
+            });
+            // will throw error if page isn't found
+          } else if(err) {
+
+            fs.readFile('./public/404.html', (err, fileContent) => {
+
+              res.writeHead(404, {'Content-Type': `text/${fileType}`, 'Content-Length': `${fileContent.length}`});
+              res.write(fileContent);
+              res.end();
+
+            });
+            // writes file if found
+          } else {
+
+             res.writeHead(200, {'Content-Type': `text/${fileType}`, 'Content-Length': `${fileContent.length}`});
+             res.write(fileContent);
+             res.end();
+
+          }
+
+        });
 
       }
 
     });
 
 
-
-
-
-    // fs.readdir('./public', (err, files) => {
-    //   console.log(files);
-    // });
-
-
-
-
-  // Client sends post data
-  if(req.method === 'POST') {
-
-    req.on('data', (data) => {
-
-      var dataPost = querystring.parse(data.toString());
-      console.log(dataPost);
-
-      var fileContents = `<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>The Elements - ${dataPost.element}</title>
-<link rel="stylesheet" href="/css/styles.css">
-<link href = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/BristolStoolChart.png/1024px-BristolStoolChart.png">
-</head>
-<body>
-<h1>${dataPost.element}</h1>
-<h2>${dataPost.elementSymbol}</h2>
-<h3>${dataPost.elementAtomicNumber}</h3>
-<p>${dataPost.elementDescription}</p>
-<p><a href="/">back</a></p>
-</body>
-</html>`
-
-
-    fs.writeFile(`./public/${dataPost.element}.html`, fileContents, (err) => {
-      if (err) throw err;
-      res.writeHead(200, {'Content-Type' : 'application/json'});
-      res.end({'success': 'true'});
-    });
-
-
-    });
-
-  }
-  // end client post
-
 });
+
+
+
+
+
+
+
 
 
 
